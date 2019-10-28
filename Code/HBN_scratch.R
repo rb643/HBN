e9ee757c0b176a5cd0c9d33e08423f893ce9e0d5
rb643 <rb643@cam.ac.uk>
2019-10-28 10:54:01 +0000

    update

--- Code/HBN_scratch.R
+++ Code/HBN_scratch.R
@@ -51,3 +51,40 @@ ggplot(data = df, aes(y = value, x = site, fill = site)) +
   theme_cowplot() +
   facet_wrap(~variable)
   dev.off()
+  
+  
+  df <- read.csv('./Data/data-2019-10-26T14_33_12.929Z.csv')  
+  df$age <- as.numeric(as.character(df$Basic_Demos.Age))
+  df <- df[!(is.na(df$age) | df$age==""), ]
+ 
+  age <- ggplot(data = df, aes(y = age, x = Basic_Demos.Sex, fill = Basic_Demos.Sex)) +
+    geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
+    geom_point(aes(y = age), position = position_jitter(width = .15), 
+               size = 1.5, alpha = 0.8) +
+    geom_boxplot(width = .1, guides = FALSE, outlier.shape = NA, alpha = 0.5) +
+    expand_limits(x = 1.5) +
+    guides(fill = FALSE) +
+    guides(color = FALSE) +
+    xlab("Sex") +
+    ylab("Age") +
+    coord_flip() +
+    theme_cowplot() 
+  
+  df$bmi <- as.numeric(as.character(df$Physical.BMI))
+  df <- df[!(is.na(df$bmi) | df$bmi==""), ]
+  
+  bmi <- ggplot(data = df, aes(y = bmi, x = Basic_Demos.Sex, fill = Basic_Demos.Sex)) +
+    geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
+    geom_point(aes(y = bmi), position = position_jitter(width = .15), 
+               size = 1.5, alpha = 0.8) +
+    geom_boxplot(width = .1, guides = FALSE, outlier.shape = NA, alpha = 0.5) +
+    expand_limits(x = 1.5) +
+    guides(fill = FALSE) +
+    guides(color = FALSE) +
+    xlab("Sex") +
+    ylab("BMI") +
+    coord_flip() +
+    theme_cowplot() 
+  
+  ggarrange(age,bmi,ncol=2)
+  
